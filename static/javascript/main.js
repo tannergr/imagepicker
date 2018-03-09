d69b5dee3d1bbd5a16b9b659d26ca1f20fd0858e
tannergr <tanner@tlgraham.com>
2018-03-08 20:57:40 -0800

    allow user to select an image

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -3,7 +3,8 @@
 
 // Client String for accessing api
 var clientString = "?client_id=" + "5ca96fac2f6435f593ce5e08a4b60ba6ef03e83e0bd55630c8bdd4fe7d2f52ef";
-
+var currentImages = [];
+var selectedImage;
 
 //Make a request to the unsplash API and update the display
 function getAndUpdate(){
@@ -17,11 +18,15 @@ function getAndUpdate(){
          // Parse response data into JSON object
          var Data = JSON.parse(this.responseText);
          Data = Data.results;
+         currentImages = Data;
          // Iterate through results
          for(i = 0; i < Data.length; i++){
            // insert image into html
-           document.getElementById("results").innerHTML += `
-            \<img src="${Data[i].urls.thumb}" class="image">
+           document.getElementById("results").innerHTML +=
+           `
+             <div id="${i.toString()}" onclick="selectImage('${i.toString()}')">
+             \<img src="${Data[i].urls.thumb}" class="image">
+             </div>
            `;
          }
 
@@ -29,4 +34,13 @@ function getAndUpdate(){
   }
   xhttp.open("GET", "https://api.unsplash.com/search/photos"+clientString+"&query="+query, true);
   xhttp.send();
+}
+
+function selectImage(index){
+  console.log("image selected");
+  if(selectedImage)
+    selectedImage.style.backgroundColor = "";
+  selectedImage = document.getElementById(index);
+  selectedImage.style.backgroundColor = "blue";
+  console.log(selectedImage);
 }
