6b154d65f2e195122dcde2b7801d556408162fd0
tannergr <tanner@tlgraham.com>
2018-03-08 20:38:04 -0800

    update js to make a search api call

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -5,30 +5,29 @@
 var clientString = "?client_id=" + "5ca96fac2f6435f593ce5e08a4b60ba6ef03e83e0bd55630c8bdd4fe7d2f52ef";
 
 window.onload = () => {
-  getAndUpdate();
 }
 
 //Make a request to the unsplash API and update the display
-function getAndUpdate(){
+function getAndUpdate(query){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange=function(){
+
     // Wait for valid response from request
     if (this.readyState == 4 && (this.status == 200)) {
-         console.log("ready")
+
          // Parse response data into JSON object
          var Data = JSON.parse(this.responseText);
+         Data = Data.results;
          // Iterate through results
          for(i = 0; i < Data.length; i++){
            // insert image into html
            document.getElementById("results").innerHTML += `
-            \<img src="${Data[i].urls.thumb}">
+            \<img src="${Data[i].urls.thumb}" class="image">
            `;
          }
 
-     } else {
-         console.log("not ready yet")
      }
   }
-  xhttp.open("GET", "https://api.unsplash.com/photos/curated"+clientString, true);
+  xhttp.open("GET", "https://api.unsplash.com/search/photos"+clientString+"&query="+query, true);
   xhttp.send();
 }
